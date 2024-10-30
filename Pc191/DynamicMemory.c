c314d571d2f0a8cedfe6cf16bc77dc59947d2d7c
Umayer <kabir20304@gmail.com>
2024-10-30 17:02:33 +0600

    Dynamic memory allocation program

---
+++ Pc191/DynamicMemory.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+ int main()
+ {
+    int n=5;
+    int *ptr;
+    ptr=(int *)malloc(n*sizeof(int));
+    if(ptr==NULL)
+    printf("NULL");
+    else 
+    {
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",(ptr+i));
+
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            printf("%d ",*(ptr+i));
+
+
+        }
+
+        printf("\n");
+
+        n=10;
+        ptr=(int *)realloc(ptr,n*sizeof(int));
+        if(ptr==NULL)
+        {
+            printf("no null");
+
+        }
+        else
+        {
+            for(int i=5;i<10;i++)
+            {
+                scanf("%d",(ptr+i));
+
+
+            }
+
+             for(int i=0;i<10;i++)
+            {
+                printf("%d ",*(ptr+i));
+                
+
+            }
+        }
+
+
+        
+    }
+
+
+
+
+  
+return 0;
+    
+ }
