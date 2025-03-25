929abcfb5610672647eb76915bbea70a5ccff801
Md Kabir Hossain <kabir20304@gmail.com>
2025-03-25 22:21:02 +0600

    negative cycle detection and Bellman-ford algorithm

--- Pc191/Graph_theory/indeg_outdeg.cpp
+++ Pc191/Graph_theory/indeg_outdeg.cpp
@@ -1,67 +1,45 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<cstring>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
 using namespace std;
-const int N=1e5;
-vector<int>adj_list[N];
+
+const int N = 1e5;
+vector<int> adj_list[N];
 int outdeg[N];
 int indeg[N];
 int visited[N];
 
-void dfs(int s)
-{
-   
-    visited[s]=1;
-
-    for(int x:adj_list[s])
-    {
-       indeg[x]++;
-       if(!visited[x])
-        dfs(x);
-
-    }
-
-}
 
 
-int main()
-{
-    memset(indeg,0,sizeof(indeg));
-    int v,e;
-    cin>>v>>e;
+int main() {
+    memset(indeg, 0, sizeof(indeg));
+    memset(outdeg, 0, sizeof(outdeg));
+    memset(visited, 0, sizeof(visited));
 
-    for(int i=0;i<e;i++)
-    {
-        int u,v;
-        cin>>u>>v;
+    int v, e;
+    cin >> v >> e;
+
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
         adj_list[u].push_back(v);
-        
+        indeg[v]++;  // Properly count in-degree while reading input
     }
 
-    for (int i=1;i<=v;i++)
-    {
-        outdeg[i]=adj_list[i].size();
-
+    for (int i = 1; i <= v; i++) {
+        outdeg[i] = adj_list[i].size();
     }
 
+   
 
-    dfs(1);
-
-
-    for(int i=1;i<=v;i++)
-    {
-        cout<<i<<" th node: indeg: "<<indeg[i]<<" \n";
-
+    for (int i = 1; i <= v; i++) {
+        cout << i << " th node: indeg: " << indeg[i] << "\n";
     }
 
-    for(int i=1;i<=v;i++)
-    {
-        cout<<i<<" th node: outdeg: "<<outdeg[i]<<" \n";
-
+    for (int i = 1; i <= v; i++) {
+        cout << i << " th node: outdeg: " << outdeg[i] << "\n";
     }
-
 
     return 0;
-
 }
