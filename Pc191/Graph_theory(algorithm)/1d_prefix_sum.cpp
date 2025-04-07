f813990493926286d55206f1486a5f120fc80cb7
Md Kabir Hossain <kabir20304@gmail.com>
2025-04-07 14:37:36 +0600

    1d prefix sum added

---
+++ Pc191/Graph_theory(algorithm)/1d_prefix_sum.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>arr;
+vector<int>parr;
+void build_prefix_sum_array(){
+    parr[0]=0;
+
+    for( int i =0 ; i< arr.size() ; i++){
+        parr[i+1]=parr[i]+arr[i];
+
+    }
+
+}
+
+int prefix_sum(int l, int r){
+    return parr[r+1]-parr[l];
+
+}
+
+int main()
+{
+    int n;
+    cin >>n;
+    arr.resize(n);
+    parr.resize(n+1);
+
+    for( int i = 0 ; i < n ; i++){
+        cin>> arr[i];
+        
+    }
+
+    build_prefix_sum_array();
+    cout<<prefix_sum(2,7)<<endl;
+    cout<<prefix_sum(0,5);
+
+
+
+
+
+
+
+   return 0;
+}
