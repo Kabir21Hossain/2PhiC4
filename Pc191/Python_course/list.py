589038f358198002e9d41292cc8fd80e882a29a0
Md Kabir Hossain <kabir20304@gmail.com>
2025-04-14 10:52:39 +0600

    list methods

--- Pc191/Python_course/list.py
+++ Pc191/Python_course/list.py
@@ -1,21 +1,22 @@
 # list,array,collection -let's say they are same
 # index =           0   1   2   3   4   5   6   7   8
-numbers=[34, 56, 2, 90, 13, 24, 56, 24, 76]
+# numbers=[34, 56, 2, 90, 13, 24, 56, 24, 76]
+
 # index =           -9  -8  -7 -6  -5  -4  -3 -2  -1
 
-print("4th index:",numbers[4])
-print("-3th index:",numbers[-3])
-print(numbers[2:6]) # start from start index and end before the end index
-print(numbers[:4])
-print(numbers[5:])
-print(numbers[1:7:1])
-print(numbers[1:8:2])
-print(numbers[::-1]) # shortcut to reverse an list
-print(numbers[::-1])
-print(numbers[7:2:-1])
-print(numbers[7:2:-2])
-print(numbers[:]) # shortcut to copy a list
-print(numbers[-4:-8:-1])
+# print("4th index:",numbers[4])
+# print("-3th index:",numbers[-3])
+# print(numbers[2:6]) # start from start index and end before the end index
+# print(numbers[:4])
+# print(numbers[5:])
+# print(numbers[1:7:1])
+# print(numbers[1:8:2])
+# print(numbers[::-1]) # shortcut to reverse an list
+# print(numbers[::-1])
+# print(numbers[7:2:-1])
+# print(numbers[7:2:-2])
+# print(numbers[:]) # shortcut to copy a list
+# print(numbers[-4:-8:-1])
 
 
 
@@ -45,8 +46,60 @@ print(numbers[-4:-8:-1])
 #     print(item)
 
 
-    
+
+# numbers=[34, 56, 2, 90, 13, 24, 56, 24, 76]
+
+# age=[]
+# for n in numbers:
+#     age.append(n)
+
+# print(age)
 
 
+# age=numbers[:]
+# print(age)
 
+# age.append(34)
+# age.append(100)
+# age.append(200)
+# age.append(700)
+# age.append(234)
+# age.append(100)
+# age.append(13)
+# print(age)
 
+# # last=age.pop()
+# # print(age)
+# # print(last)
+# if 76 in age:
+#     age.remove(76)
+# print(age)
+
+# if 1000 in age:
+#     age.remove(1000)
+# print(age)
+
+# age.remove(2000)
+# print(age)
+# print(len(age))
+# age.insert(8,1000000)
+# age.insert(0,0)
+# age.insert(len(age),0)
+# print(age)
+
+# age=numbers[::-1]
+# print(age)
+# age.clear()
+# print("printing age:",age)
+
+player=[2,2,1,4,5,1,2,3,5,4,4,12,1,4,3,2,5]
+n=player.count(2)
+print(n)
+print(player.index(12))
+player.sort()
+print(player)
+player.reverse()
+print(player)
+l=[]
+l=player.copy()
+print("printing l:",l)
---
+++ Pc191/Python_course/list2.py
@@ -0,0 +1,55 @@
+#list as stack
+# stack=[3,4,5,6]
+# stack.append(4)
+# stack.append(45)
+# stack.append(100)
+# stack.append(200)
+# print(stack)
+# print(stack.pop())
+
+# list as queue
+
+from collections import deque
+queue=deque(["kabir","rahim","shafiq","anand","reza"])
+print(queue)
+queue.append("raja")
+queue.append("rajendra")
+print(queue)
+
+print(queue.popleft())
+
+# list comprehension
+
+# number=[1,2,3,4,5,6,7,8,9,10]
+
+# for n in number:
+#     if n%2==0:
+#         print(n)
+
+# del number[0]
+# print(number)
+# del number
+# print(number)
+
+numbers=[45,87,96,65,43,85,14,26,61,90,70]
+odds=[]
+for num in numbers:
+    if num%2==1 and num%5==0:
+        odds.append(num)
+
+print(odds)
+
+odd_nums=[num for num in numbers if num%2 ==1 and num%5==0]
+print(odd_nums)
+
+squares= [x**2 for x in range(6)]
+print(squares)
+
+players=['sakib','mushfik','liton']
+age=[32,14,19]
+age_comb=[]
+for p in players:
+    for a in age:
+       age_comb.append([a,p])
+
+print(age_comb)
