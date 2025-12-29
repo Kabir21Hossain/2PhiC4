2ef7491608cd7bb0ce19da5130173174e5978525
Md Kabir Hossain <kabir20304@gmail.com>
2025-12-29 14:47:26 +0600

    Trie data Structure with insertion and search operations are done

---
+++ Pc191/LeetCode/Trie-DataStructure/insert_word.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.EOF=False
+
+class Trie:
+    def __init__(self):
+        self.root=TrieNode()
+    
+    def insert(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char]=TrieNode()
+            node=node.children[char]
+            
+        node.EOF=True
+    
+    def search(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                return False
+        
+            node=node.children[char]
+        
+        return node.EOF
+    
+    def starts_with(self,prefix):
+        node=self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node=node.children[char]
+        
+        return True
+    
+    
+        
+    
